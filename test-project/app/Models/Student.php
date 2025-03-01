324bfe5159057833c9637700a215a06490b956c4
yonas-jemberu-kebede <jemberuyonas01@gmail.com>
2025-03-01 16:22:37 +0300

    student_subject migration file

---
+++ test-project/app/Models/Student.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $guarded=[];
+
+    public function subjects()
+    {
+        return $this->belongsToMany(Subject::class, 'student_subject');
+    }
+    public function teachers()
+    {
+        return $this->belongsToMany( Teacher::class, 'student_teacher');
+    }
+}
--- test-project/app/Models/Teacher.php
+++ test-project/app/Models/Teacher.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Teacher extends Model
 {
     use HasFactory;
+    protected $guarded=[];
+    public function subjects(){
+        return $this->belongsToMany(Subject::class,'subject_teacher');
+    }
+    public function students()
+    {
+        return $this->belongsToMany(Student::class, 'student_teacher');
+    }
+
 }
